739c657bef725d588ff9a01be3067593f886e1ae
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-06-06 15:50:02 -0300

    1035

---
+++ Iniciante/1035.js
@@ -0,0 +1,12 @@
+const valoresT = [2, 2, 2, 2]
+
+var a = valoresT.shift()
+var b = valoresT.shift()
+var c = valoresT.shift()
+var d = valoresT.shift()
+
+if ((b > c && d > a) && ((c + d) > (a + b)) && ((c > 0) && (d > 0)) && ((a % 2) === 0)) {
+    console.log('Valores aceitos')
+} else {
+    console.log('Valores não aceitos')
+}
