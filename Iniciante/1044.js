c901e4993b21308cfc12007562f510818e14adb3
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-06-24 13:17:31 -0300

    1044 e 1045

--- Iniciante/1044.js
+++ Iniciante/1044.js
@@ -3,3 +3,9 @@ let lines = input.split('\n');
 
 let [A, B] = lines[0].split(' ').map(Number);
 
+if (A % B === 0 || B % A === 0) {
+    console.log(`Sao Multiplos`);
+} else {
+    console.log(`Nao sao Multiplos`);
+}
+
