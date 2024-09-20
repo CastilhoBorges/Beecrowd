43f440eb45dcaf28db027e48a2a8172701bf8441
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-09-20 17:37:12 -0300

    1067

--- Iniciante/1067.js
+++ Iniciante/1067.js
@@ -1,4 +1,10 @@
 "use strict";
 
+let input = require("fs").readFileSync("1067", "utf8");
+let lines = input.split("\n");
 
+const number = Number(lines[0]);
 
+for (let i = 1; i <= number; i++) {
+  if (i % 2 === 1) console.log(i);
+}
