7292d7e17d362b31fbd4b75c5f5be7b9f8a689c3
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-09-25 11:06:36 -0300

    Finalizando o desafio 1098

--- Iniciante/1098.js
+++ Iniciante/1098.js
@@ -3,10 +3,10 @@ for (
   i <= 2;
   i += 0.2, j1 += 0.2, j2 += 0.2, j3 += 0.2
 ) {
-  if (i === 0 || i === 1 || i === 2.) {
-    console.log(`I=${i} J=${j1}`);
-    console.log(`I=${i} J=${j2}`);
-    console.log(`I=${i} J=${j3}`);
+  if (i === 0 || i === 1 || i >= 1.9) {
+    console.log(`I=${Math.round(i)} J=${Math.round(j1)}`);
+    console.log(`I=${Math.round(i)} J=${Math.round(j2)}`);
+    console.log(`I=${Math.round(i)} J=${Math.round(j3)}`);
   } else {
     console.log(`I=${i.toFixed(1)} J=${j1.toFixed(1)}`);
     console.log(`I=${i.toFixed(1)} J=${j2.toFixed(1)}`);
