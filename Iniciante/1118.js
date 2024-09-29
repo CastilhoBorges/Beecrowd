be2db02e35e73c2ca9907a838c7748db7c5c03a6
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-09-28 21:10:21 -0300

    Tentativa

--- Iniciante/1118.js
+++ Iniciante/1118.js
@@ -1,21 +1,20 @@
 let input = require("fs").readFileSync("1118", "utf8");
 let lines = input.split("\n");
 
-let notas = [];
-let inputs = [];
+const arr = lines.map(Number);
+const notas = [];
 
-for (const i of lines) {
-  if (i.includes(".")) {
-    notas.push(i);
+for (const i of arr) {
+  if (i < 0 || i > 10) {
+    console.log("nota invalida");
   } else {
-    inputs.push(i);
+    notas.push(i);
+    arr.shift();
   }
 }
+console.log(arr);
+console.log(notas);
+console.log(`media = ${(notas[0] + notas[1]) / 2}`);
 
-inputs = inputs.map(Number);
-
-for (const i of notas) {
-  if (i >= 0 && i <= 10) {
-    console.log(`media = ${(notas[0] + notas[1]) / 2}`);
-  }
-}
+notas.shift();
+notas.shift();
