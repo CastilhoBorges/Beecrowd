101a9eb1a3641fc833f8c8fd8fdd2e68cb8c24c0
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-10-21 17:18:25 -0300

    Tentar amanha

--- Iniciante/1149.js
+++ Iniciante/1149.js
@@ -1,17 +1,8 @@
 let input = require("fs").readFileSync("1149", "utf8");
 let lines = input.split(" ").map(Number);
 
-let a = lines.shift();
-let sum = a;
+const a = lines[0]; 
 
-for (const x of lines) {
-  if (x > 0) {
-    for (let i = 0; i <= x - 1; i++) {
-      sum += i;
-    }
-  } else {
-    continue;
-  }
+for (let i = 0; i <= n - 1; i++) {
+
 }
-
-console.log(a + sum);
